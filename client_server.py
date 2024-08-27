ab768227184a850479d2527b37745d9830d6e9c1
jaeminHw <bosik99@naver.com>
2024-08-27 17:21:44 +0900

    client_socket_connection

---
+++ client_server.py
@@ -0,0 +1,27 @@
+import main
+
+# main.main()
+
+import socketio
+
+sio = socketio.Client()
+
+@sio.event
+def connect():
+    print("I'm connected!")
+
+@sio.event
+def disconnect():
+    print("I'm disconnected!")
+
+@sio.event
+def sensor_start():
+    print("sensor_start")  # 서버로부터 받은 메시지 출력
+
+sio.connect('http://192.168.0.72:5000')
+
+try:
+    # 클라이언트가 계속 실행되도록 합니다.
+    sio.wait()
+except KeyboardInterrupt:
+    sio.disconnect()
